91b2728191d8630f66dce910643f3a109a0bdfee
Benoît <benoitobelia@gmail.com>
2022-03-02 12:33:06 +0100

    Fix profile bug

--- flaskr/user.py
+++ flaskr/user.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 bp = Blueprint("user", __name__, url_prefix="/user")
 
-@bp.route('/') #<str:username> g.user['username']
+@bp.route('/<username>') #<str:username> g.user['username']
 def profile(username:str):
     """Show profile of a user"""
-    return render_template("/user/profile.html")
+    return render_template("/user/profile.html", username=username)
