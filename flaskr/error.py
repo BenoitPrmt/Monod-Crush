e811ef775c4e562bfb30173834a4866e1881442a
KillianTib <killiant53@gmail.com>
2022-03-06 21:52:31 +0100

    error page

--- flaskr/error.py
+++ flaskr/error.py
@@ -1,9 +1,9 @@
-# from flask import Blueprint, flash, g, redirect, render_template, request, url_for
-# from werkzeug.exceptions import abort
-#
-# bp = Blueprint("error", __name__, url_prefix="/error")
-#
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     # note that we set the 404 status explicitly
-#     return render_template('/error/error_404.html'), 404
+from flask import Flask, render_template 
+  
+app = Flask(__name__) 
+  
+@app.errorhandler(404) 
+  
+def not_found(e): 
+  
+  return render_template("error_404.html") 
