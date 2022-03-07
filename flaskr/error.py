8a4b50dbd718781ffd2e8ebae8dcf357fe19e03a
KillianTib <killiant53@gmail.com>
2022-03-07 20:20:15 +0100

    error page optimization

--- flaskr/error.py
+++ flaskr/error.py
@@ -1,6 +1,11 @@
 from flask import render_template 
-  
+
 def not_found(e): 
-  
-  return render_template("error/all_error.html", error_code = 404) #a modifier pour recuperer le numéro de l'erreur
+  '''retourner le numéro de l'erreur et afficher la page all_error.html'''
+
+  number = []
+  e = str(e).split(" ") #split e pour isoler le numéro
+  number.append(e)
+
+  return render_template("error/all_error.html", error_code = e[0]) #revoi la page d'erreur et e[0] = numéro erreur
 
