4a954f68cc104a817b39f928af7add86d1b95251
Jules <77736557+JulesGrd@users.noreply.github.com>
2022-03-21 20:50:36 +0100

    log error

--- flaskr/error.py
+++ flaskr/error.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import render_template, app, current_app, request
 from werkzeug.exceptions import HTTPException
+
 
 def error_handler(error: HTTPException):
     """ Error handler for HTTP exceptions. """
+
+    current_app.logger.warning(f"Error {error.code}: {request.url} by {request.remote_addr}")
+
     return render_template("error/all_error.html", error_code=error.code), error.code
