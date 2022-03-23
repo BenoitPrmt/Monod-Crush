3727e7269b4448a31c826a7de391b0d6e1c3519a
Jules <77736557+JulesGrd@users.noreply.github.com>
2022-03-24 00:30:49 +0100

    fix logging file format and waring log

--- flaskr/error.py
+++ flaskr/error.py
@@ -5,6 +5,6 @@ from werkzeug.exceptions import HTTPException
 def error_handler(error: HTTPException):
     """ Error handler for HTTP exceptions. """
 
-    current_app.logger.warning(f"Error {error.code}: {request.url} by {request.remote_addr}")
+    current_app.logger.warning(f"{request.remote_addr} {error.code} '{request.method} {request.url}' {request.data}")
 
     return render_template("error/all_error.html", error_code=error.code), error.code
