9a5d1013b9cff46f8ec07e0b2e6698ed36b44b9c
KillianTib <killiant53@gmail.com>
2022-03-26 17:15:47 +0100

    admin panel messages

--- flaskr/admin.py
+++ flaskr/admin.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
-
-from flaskr.auth_helper import admin_only
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for, Response, current_app
+from flaskr.blog_helper import get_post, check_message_body
+from flaskr.auth_helper import admin_only, login_required
 from flaskr.db import get_db
+import locale
+from typing import Union
+
+
 
 bp = Blueprint("admin", __name__, url_prefix="/admin")
 
@@ -14,8 +18,53 @@ def panel() -> str:
     db = get_db()
     users = db.execute("SELECT COUNT(id) FROM user").fetchone()
     admins = db.execute("SELECT COUNT(id) FROM user WHERE admin = 1").fetchone()
-    posts = db.execute("SELECT COUNT(id) FROM post").fetchone()
+    nb_posts = db.execute("SELECT COUNT(id) FROM post").fetchone()
     comments = db.execute("SELECT COUNT(id) FROM comment").fetchone()
+    posts = db.execute("""
+        SELECT p.id, p.body, p.status,p.anonymous, p.created, p.author_id, u.username
+        FROM post p JOIN user u ON p.author_id = u.id
+        WHERE p.reported != ''
+        ORDER BY p.created DESC
+        
+        """).fetchall()
+    print(posts)
+    return render_template("/admin/panel.html", nb_posts=nb_posts[0], users=users[0],
+                           admins=admins[0], comments=comments[0], posts=posts)
 
-    return render_template("/admin/panel.html", posts=posts[0], users=users[0],
-                           admins=admins[0], comments=comments[0])
+
+
+@bp.route("/post/<int:post_id>/delete", methods=["POST"])
+@admin_only
+def delete(post_id: int) -> Response:
+    """Delete a post.
+
+    Ensures that the post exists and that the logged in user is the
+    author of the post.
+    """
+    get_post(post_id)
+
+    db = get_db()
+    db.execute("DELETE FROM post WHERE id = ?", (post_id,))
+    db.commit()
+
+    current_app.logger.info(f"{g.user['id']} ({g.user['username']}) - deleted post {post_id}")
+
+    return redirect(url_for("admin.panel"))
+
+
+@bp.route("/post/<int:post_id>/checking", methods=["POST"])
+@admin_only
+def checking(post_id: int) -> Response:
+    
+    get_post(post_id, check_author=False)
+
+    db = get_db()
+    r = db.execute("SELECT reported, status FROM post WHERE id = ?", (post_id,)).fetchone()
+    db.execute("UPDATE post SET reported = NULL WHERE id = ?", (post_id,)).fetchone()
+    
+    
+    if r["status"] == "hidden":
+        db.execute("UPDATE post SET status = 'visible' WHERE id = ?", (post_id,))
+
+    db.commit()
+    return redirect(url_for("admin.panel"))
