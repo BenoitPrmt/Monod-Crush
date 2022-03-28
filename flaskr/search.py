8127b031d0a8a5891585f2d553adda7ee7fd69f3
KillianTib <killiant53@gmail.com>
2022-03-29 00:02:49 +0200

    search bar

--- flaskr/search.py
+++ flaskr/search.py
@@ -1,3 +1,4 @@
+from re import search
 from flask import Blueprint, flash, redirect, render_template, request, session, url_for, current_app, g
 from datetime import date
 
@@ -5,11 +6,21 @@ from werkzeug.security import generate_password_hash
 from flaskr.auth_helper import login_required, check_password_strength, check_username
 from flaskr.db import get_db
 
-bp = Blueprint("user", __name__, url_prefix="/user") #a faire jspl quoi mettre dedans
+bp = Blueprint("search", __name__) #a faire jspl quoi mettre dedans
 
-@bp.route("/search", methods=("GET"))
-def serach_user():
+@bp.route("/search")
+def search_user():
     """search user and return best result on search_user.html"""
-    
 
-    return render_template("search/search_user.html")
+    db = get_db()
+    username = request.args["search_user"]
+    
+    username = str(username)
+    print(username)
+    users = db.execute("SELECT admin FROM user WHERE username = ?", (username,)).fetchone()
+    if users is None:
+        print(users)
+        return render_template("search/search_user.html", user="None")
+    else:
+        print(users)
+        return render_template("search/search_user.html", user=username)
