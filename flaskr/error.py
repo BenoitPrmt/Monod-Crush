92ddde394507c93413d979368fa2bf68ccdc18ba
Jules <77736557+JulesGrd@users.noreply.github.com>
2022-03-29 12:22:25 +0200

    new style in 403 page 🚨

--- flaskr/error.py
+++ flaskr/error.py
@@ -1,21 +1,34 @@
-from flask import render_template, current_app, request, redirect
+from flask import render_template, current_app, request, redirect, g
 from werkzeug.exceptions import HTTPException
+
+
+def get_user_or_ip() -> str:
+    """
+    Returns the user or the IP address of the client
+    """
+    if g.user is not None:
+        return f"{g.user['id']} ({g.user['username']})"
+
+    return request.headers.get('X-Real-IP', request.remote_addr)
 
 
 def error_handler(error: HTTPException):
     """ Error handler for HTTP exceptions. """
 
-    if error.code == 500:
-        current_app.logger.error(f"{request.remote_addr} - {error.code}"
-                                 f" '{request.method} {request.url} {request.headers}'")
-
     # redirect foo/bar/something/  to foo/bar/something
     # check if not end with foo/bar////////// for not enter in loop
-    elif error.code == 404 and request.path.endswith('/') and not request.path.endswith('//'):
+    if error.code == 404 and request.path.endswith('/') and not request.path.endswith('//'):
         return redirect(request.path[:-1])
 
+    elif error.code == 403:
+        current_app.logger.error(f"{get_user_or_ip()} - 403 '{request.method} {request.path}'")
+        return render_template('error/403_forbidden.html'), 403
+
+    elif error.code == 500:
+        current_app.logger.error(
+            f"{get_user_or_ip()} - {error.code} '{request.method} {request.path} {request.headers}'")
+
     else:
-        current_app.logger.warning(
-            f"{request.headers.get('X-Real-IP', request.remote_addr)} - {error.code} '{request.method} {request.url}'")
+        current_app.logger.warning(f"{get_user_or_ip()} - {error.code} '{request.method} {request.path}'")
 
     return render_template("error/error_base.html", error_code=error.code, error_message=error.description), error.code
