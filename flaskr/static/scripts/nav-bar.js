6023586cb2b4ca9da72b3e91bd6d2b671a598634
Jules <77736557+JulesGrd@users.noreply.github.com>
2022-03-29 11:44:55 +0200

    fix search bar and mobile dropdown bug

--- flaskr/static/scripts/nav-bar.js
+++ flaskr/static/scripts/nav-bar.js
@@ -1,25 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    // Get all "navbar-burger" elements
+    const burgerButton = document.querySelector('.navbar-burger');
+    const navbarMenu = document.querySelector('.navbar-menu');
 
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
+    const dropdownContainer = document.querySelector('.navbar-item.has-dropdown');
+    const dropdownMenu = document.querySelector('.navbar-dropdown');
 
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
+    window.onresize = function() {
+        if (window.innerWidth > 1025) {
+            navbarMenu.classList.remove('is-active');
+            burgerButton.classList.remove('is-active');
+            dropdownMenu.classList.remove('is-hidden');
+        }
+    }
 
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
+    if (burgerButton && navbarMenu) {
+        burgerButton.addEventListener('click', () => {
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            burgerButton.classList.toggle('is-active');
+            navbarMenu.classList.toggle('is-active');
 
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
+            if (burgerButton.classList.contains('is-active')) {
+                // en ouvrant le menu on cache le dropdown
+                dropdownMenu.classList.add('is-hidden');
+            } else {
+                // en fermant le menu, on remet le dropdown menu en place
+                dropdownMenu.classList.remove('is-hidden');
+            }
+        });
+    }
 
-      });
+
+
+    dropdownContainer.addEventListener('click', () => {
+        if (burgerButton.classList.contains('is-active')) {
+
+            if (burgerButton.classList.contains('is-active')) {
+                dropdownMenu.classList.toggle('is-hidden');
+            }
+        }
+
     });
-  }
-
 });
